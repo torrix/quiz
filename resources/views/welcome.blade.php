3c749c95cb49551f4093bb9228df7142e1319cf5
Matt Fletcher <matt@torrix.uk>
2020-04-13 15:36:02 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>General Knowledge</h1>
+    <h2>Question 1</h2>
+    <p>
+        Who is next in line to the British throne?
+    </p>
+    <div class="uk-alert uk-alert-success">
+        Prince Charles
+    </div>
+@endsection
